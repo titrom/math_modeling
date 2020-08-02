4da2292bacd1aaaf21c78a557f3692ae66335f87
pavloova <67763277+pavloova@users.noreply.github.com>
2020-08-03 02:00:20 +0300

    Update lab63.py

--- lab63.py
+++ lab63.py
@@ -13,11 +13,11 @@ def sinus(frame, A=0.5,f=2,a=1,F=1):
     t= np.linspace(0,frame,100)
     xdata.append(f*t)
     ydata.append(A*np.sin(f*t))
-    anim_object.set_data(xdata,ydata)
+    anim_object1.set_data(xdata,ydata)
 
-    xdata.append(F*t)
-    ydata.append(a*np.sin(F*t))
-    anim_object.set_data(xdata,ydata)
+    x1.append(F*t)
+    y2.append(a*np.sin(F*t))
+    anim_object.set_data(x1,y2)
     return anim_object1,anim_object,
 
 ani =FuncAnimation(fig,sinus,frames = np.linspace(0,2*np.pi,100),interval = 100)
