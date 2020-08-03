4f7238e28ecfa3d529786932a7b6a887163f8c1c
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2020-08-03 17:11:44 +0200

    Теперь все работает!

--- lab63.py
+++ lab63.py
@@ -8,14 +8,16 @@ xdata, ydata = [],[]
 ax.set_xlim(0, 2*np.pi)
 ax.set_ylim(-1, 1)
 
+y2 = []
+x1 = []
 def sinus(frame, A=0.5,f=2,a=1,F=1):
    
     t= np.linspace(0,frame,100)
-    xdata.append(f*t)
+    xdata.append(t)
     ydata.append(A*np.sin(f*t))
     anim_object1.set_data(xdata,ydata)
 
-    x1.append(F*t)
+    x1.append(t)
     y2.append(a*np.sin(F*t))
     anim_object.set_data(x1,y2)
     return anim_object1,anim_object,
