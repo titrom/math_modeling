c2d64db31dbb9c979af6514a1ef82a450d5ff011
titrom <71824652+titrom@users.noreply.github.com>
2020-09-24 17:16:24 +0200

    введение в синтексис

---
+++ lec1_type_data.py
@@ -0,0 +1,35 @@
+print(type(3.4))
+
+print(type('Привет'))
+
+print('привет')
+
+a='привет'
+
+print(a)
+
+print(type([1,2,3]))
+
+a='Good'
+b='bad'
+
+print(a+b)
+
+a=[1, 3, 6] #создание списка
+
+print(a[0]) #вывод наэкран элемента из списка
+
+b=[8, 10, 11] 
+
+c=a+b
+
+print(c) 
+
+A=[]
+A.append(1)
+
+print(A)
+ 
+A.append(b)
+
+print(A)
