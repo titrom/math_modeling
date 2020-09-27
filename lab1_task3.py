8c5769e8c178033d610e4167d329476bd70c8e72
titrom <71824652+titrom@users.noreply.github.com>
2020-09-28 00:54:52 +0200

    задача 3

---
+++ lab1_task3.py
@@ -0,0 +1,29 @@
+print('Номер 1)')
+
+import math
+
+x=3
+
+a=x**3+3/3
+
+b=4*x**7-x**5
+
+c=80*math.sqrt(27*x**4+12*x**3-5*x**2+10)
+
+y=math.sqrt(x**3)/a*b+c
+
+print(y)
+
+print('Номер 2)')
+
+b=16.7*4.32//1
+
+a=3%2+b
+g=x**3.4//1
+d=31%12-g
+
+c=14.5+d
+
+y=a/c
+
+print(y)
