d47d95c11632cb88219b5a6918d1c5e5c026efbb
titrom <71824652+titrom@users.noreply.github.com>
2020-09-29 17:51:45 +0200

    новая инфа

---
+++ lec2_if.py
@@ -0,0 +1,16 @@
+
+if 1: #начоло
+    print('hello 1') #true
+
+a=int(input('Введите число: '))
+
+
+if a>1:
+    print('hello',a)
+
+
+b=int(input('Введите число: '))
+if b==5:
+    print('hello')
+     
+    
