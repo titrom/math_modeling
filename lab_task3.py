2aab39710d519c0f8cecc0e9b98a54ba5ad81bd6
titrom <71824652+titrom@users.noreply.github.com>
2020-10-06 17:19:02 +0200

    доп

---
+++ lab_task3.py
@@ -0,0 +1,6 @@
+a=input('введите число: ')
+t=''
+for b in range(len(a)-1,-1,-1):
+    t+=a[b]
+print(t)
+    
---
+++ lab_task7.py
@@ -0,0 +1,9 @@
+for i in range(1,10,1):
+    for a in range (1,10,1):
+        print(i*a,end=' ')
+    print()
+
+
+
+    
+    
