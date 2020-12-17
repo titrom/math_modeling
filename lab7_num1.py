4e06af032c26d0b65ff8e951b319358f13739d0d
titrom <71824652+titrom@users.noreply.github.com>
2020-12-17 16:23:20 +0200

    LABA7№2

---
+++ lab7_num1.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+n=int(input())
+def func1(R):
+    t=np.arange(-2*np.pi,2*np.pi,0.01)
+    x=R*(t-np.sin(t)**3)
+    y=R*(1-np.cos(t)**3)
+    plt.axis('equal')
+    plt.plot(x,y)
+    plt.show()
+def func2(R):
+    t=np.arange(-2*np.pi,2*np.pi,0.01)
+    x=R*np.cos(t)**3
+    y=R*np.sin(t)**3
+    plt.axis('equal')
+    plt.plot(x,y)
+    plt.show()
+if n==1:
+    func1(10)
+elif n==2:
+    func2(2)
+    
+    
+    
