f72f405facc25e3d450958e045247c1ab51fc59d
titrom <71824652+titrom@users.noreply.github.com>
2020-12-17 17:13:30 +0200

    Update lab7_num2.py

--- lab7_num2.py
+++ lab7_num2.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib.animation  import FuncAnimation
 fig,ax=plt.subplots()
 ball,=plt.plot([],[],'o',color='r')
-def cirle_move(R,time):
+def cirle_move(R):
 
     alpha=np.arange(0,2*np.pi,0.1)
-    x=R*np.cos(alpha)*time
-    y=R*np.sin(alpha)*time
+    x=R*np.cos(alpha)
+    y=R*np.sin(alpha)
     return x,y
 edge=15
 plt.axis('equal')
 ax.set_xlim(-edge,edge)
 ax.set_ylim(-edge,edge)    
 def animate(i):
-    ball.set_data(cirle_move(R=1,time=i))
+    ball.set_data(cirle_move(R=i))
 ani=FuncAnimation(fig,
                   animate,
                   frames=100,
