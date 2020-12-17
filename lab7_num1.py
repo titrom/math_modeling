3929f2d5ea97e41da64b2462024de91561f352c1
titrom <71824652+titrom@users.noreply.github.com>
2020-12-17 17:12:18 +0200

    Update lab7_num1.py

--- lab7_num1.py
+++ lab7_num1.py
@@ -3,8 +3,8 @@ import numpy as np
 n=int(input())
 def func1(R):
     t=np.arange(-2*np.pi,2*np.pi,0.01)
-    x=R*(t-np.sin(t)**3)
-    y=R*(1-np.cos(t)**3)
+    x=R*(t-np.sin(t))
+    y=R*(1-np.cos(t))
     plt.axis('equal')
     plt.plot(x,y)
     plt.show()
